2f8c9667d78d48a81a22ac9d3d7e10fd959c5a58
Darius2708 <166720048+Darius2708@users.noreply.github.com>
2024-10-05 15:45:07 +0300

    Delete proiect-Gourmet/proiect directory

--- proiect-Gourmet/proiect/scroll.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function(){
-    $('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
-    });
-
-$(window).scroll(function(){
-    $('a').toggleClass('scrolled', $(this).scrollTop() > 100);
-    });
